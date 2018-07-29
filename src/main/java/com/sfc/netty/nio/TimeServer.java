b07d7f1a863f4043c4b2e6c161e24bf6e21c2294
slowfishcolor <slowfishcolor@gmail.com>
2018-07-29 21:02:34 +0800

    Add NIO time server implementation.

---
+++ src/main/java/com/sfc/netty/nio/TimeServer.java
@@ -0,0 +1,17 @@
+package com.sfc.netty.nio;
+
+public class TimeServer {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Using default port: " + port);
+            }
+        }
+        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+        new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
+    }
+}
