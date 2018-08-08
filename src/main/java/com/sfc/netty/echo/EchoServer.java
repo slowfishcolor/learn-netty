ceb0e68337bc3d11c2eae4346fc409501de66245
slowfishcolor <slowfishcolor@gmail.com>
2018-08-08 23:34:49 +0800

    Add netty hello world, an echo server

---
+++ src/main/java/com/sfc/netty/echo/EchoServer.java
@@ -0,0 +1,61 @@
+package com.sfc.netty.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("Usage: " + EchoServer.class.getSimpleName() + " <port>");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    public void start() throws Exception {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        // 创建 EventLoopGroup
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            // 创建 ServerBootstrap
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    // 指定所使用的 NIO 传输 Channel
+                    .channel(NioServerSocketChannel.class)
+                    // 使用指定的端口设置套接字地址
+                    .localAddress(new InetSocketAddress(port))
+                    // 添加一个EchoServerHandler到于Channel的 ChannelPipeline
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            // EchoServerHandler 被标注为@Shareable，所以我们可以总是使用同样的实例
+                            // 这里对于所有的客户端连接来说，都会使用同一个 EchoServerHandler，因为其被标注为 @Sharable
+                            ch.pipeline().addLast(serverHandler);
+                        }
+                    });
+            // 异步地绑定服务器；调用 sync()方法阻塞等待直到绑定完成
+            ChannelFuture f = b.bind().sync();
+            System.out.println(EchoServer.class.getName() + " started and listening for connections on " + f.channel().localAddress());
+            // 获取 Channel 的CloseFuture，并且阻塞当前线程直到它完成
+            f.channel().closeFuture().sync();
+        } finally {
+            // 关闭 EventLoopGroup，释放所有的资源
+            group.shutdownGracefully().sync();
+        }
+    }
+}
